2ff2281174f42d023f6767dc1e8275aabb136d23
Mosesomo <moseswamboga4@gmail.com>
2023-11-26 06:56:39 -0800

    puppet

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,7 @@
+# configuring the ssh file
+
+exec { 'echo':
+  path    => 'usr/bin:/bin',
+  command => 'echo "    IdentityFile ~/.ssh/school\n    PasswordAuthentication no" >> /etc/ssh/ssh_config',
+  returns => [0,1],
+}
