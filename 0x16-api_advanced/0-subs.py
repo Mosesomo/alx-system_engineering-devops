16710b8815ade856591f9a1add748d00529d9151
Mosesomo <moseswamboga4@gmail.com>
2024-01-16 19:04:16 +0300

    How many subs?

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import requests
+
+
+def number_of_subscribers(subreddit):
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'User-Agent': 'my_app/1.0'}
+    response = requests.get(url=url, headers=headers)
+    if response.status_code >= 300:
+        return 0
+    data = response.json()
+    return data['data']['subscribers']
