05aa2b7ed7c531f2e33b9b0a6925906e62378bba
Mosesomo <moseswamboga4@gmail.com>
2024-01-17 18:17:58 +0300

    corrections

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,11 +7,11 @@ def top_ten(subreddit):
     """function that queries the Reddit API and prints the
         titles of the first 10 hot posts listed for a given subreddit.
     """
-    
+
     info = requests.get("https://www.reddit.com/r/{}/hot.json?limit=10"
-                            .format(subreddit),
-                            headers={"User-Agent": "My-User-Agent"},
-                            allow_redirects=False)
+                        .format(subreddit),
+                        headers={"User-Agent": "My-User-Agent"},
+                        allow_redirects=False)
     if info.status_code >= 300:
         print('None')
     else:
