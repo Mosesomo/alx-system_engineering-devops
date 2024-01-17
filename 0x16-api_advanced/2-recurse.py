eaa4dca31b54b116756d42a5df7d250c08ad256b
Mosesomo <moseswamboga4@gmail.com>
2024-01-17 19:18:14 +0300

    recurse

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -5,7 +5,8 @@ import requests
 
 def recurse(subreddit, hot_list=[], limit=0, after=None):
     """recursive function that queries the Reddit API and
-        returns a list containing the titles of all hot articles for a given subreddit
+        returns a list containing the titles of all hot
+        articles for a given subreddit
     """
 
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
