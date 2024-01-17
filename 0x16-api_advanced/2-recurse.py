99a74f41728ad1561bb5e8445220ae7c1a0914bd
Mosesomo <moseswamboga4@gmail.com>
2024-01-17 20:09:17 +0300

    recurse

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -3,18 +3,18 @@
 import requests
 
 
-def recurse(subreddit, hot_list=[], limit=0, after=None):
+def recurse(subreddit, hot_list=[], count=0, after=None):
     """recursive function that queries the Reddit API and
         returns a list containing the titles of all hot
     """
 
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    params = {"count": limit, "after": after}
+    params = {"count": count, "after": after}
     headers = {"User-Agent": "My-User-Agent"}
     response = requests.get(url, params=params, headers=headers)
 
     if response.status_code >= 400:
-        print("None")
+        return None
     data = response.json()
     hot = hot_list + [post.get('data').get("title")
                       for post in data.get('data').get('children')]
